baf0129fb68e7fc8d521308e40bddc8c2bbb20a1
Blair MacIntyre <blair@macmynatt.com>
2021-09-23 10:35:52 -0400

    updated index to have all shaders

--- src/rooms/index.ts
+++ src/rooms/index.ts
@@ -2,12 +2,16 @@ import '../systems/fader-plus.js'
 import '../components/portal.js'
 import '../components/immersive-360.js'
 import '../components/parallax.js'
+import '../components/shader.ts'
 import '../components/html-script.js'
+import '../components/region-hider.js'
 
 AFRAME.GLTFModelPlus.registerComponent('immersive-360', 'immersive-360')
 AFRAME.GLTFModelPlus.registerComponent('portal', 'portal')
+AFRAME.GLTFModelPlus.registerComponent('shader', 'shader')
 AFRAME.GLTFModelPlus.registerComponent('parallax', 'parallax')
 AFRAME.GLTFModelPlus.registerComponent('html-script', 'html-script')
+AFRAME.GLTFModelPlus.registerComponent('region-hider', 'region-hider')
 
 // do a simple monkey patch to see if it works
 
